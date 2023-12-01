9a99b73a9d1bc4d59ddb0385f0e19d32d61c3b7c
Stephen Watson <svenjwatson@gmail.com>
2023-12-01 15:16:21 +1030

    (Utility) Passing tests to handle string case

--- Tests/InputParserTests.py
+++ Tests/InputParserTests.py
@@ -1,19 +1,19 @@
 import Utilities.InputParser
 
 def test_GetListWithoutDelimiter():
-    assert Utilities.InputParser.ExtractList("TestData/nodelimiterlist.txt", "") == [1, 2, 3, 4, 5, 6]
+    assert Utilities.InputParser.ExtractListWithIntValues("TestData/nodelimiterlist.txt", "") == [1, 2, 3, 4, 5, 6]
 
 def test_GetListWithCommaDelimiter():
-    assert Utilities.InputParser.ExtractList("TestData/commaseparatedlist.txt", ",") == [1, 2, 3, 4, 5, 6, 7]
+    assert Utilities.InputParser.ExtractListWithIntValues("TestData/commaseparatedlist.txt", ",") == [1, 2, 3, 4, 5, 6, 7]
 
 def test_GetListWithSpaceDelimiter():
-    assert Utilities.InputParser.ExtractList("TestData/spacedelimiter.txt", " ") == [1, 2, 3, 4]
+    assert Utilities.InputParser.ExtractListWithIntValues("TestData/spacedelimiter.txt", " ") == [1, 2, 3, 4]
 
 def test_GetListWithSpaceDelimiterAsStrings():
     assert Utilities.InputParser.ExtractListWithStringValues("TestData/spacedelimiter.txt", " ") == ["1", "2", "3", "4"]
 
 def test_GetListWithCommaDelimiterAsStrings():
-    assert Utilities.InputParser.ExtractListWithStringValues("TestData/commaseparatedlist.txt", " ") == ["1", "2", "3", "4", "5", "6", "7"]
+    assert Utilities.InputParser.ExtractListWithStringValues("TestData/commaseparatedlist.txt", ",") == ["1", "2", "3", "4", "5", "6", "7"]
 
 def test_GetListWithNoDelimiterAsStrings():
-    assert Utilities.InputParser.ExtractListWithStringValues("TestData/nodelimiterlist.txt", " ") == ["1", "2", "3", "4", "5", "6"]
+    assert Utilities.InputParser.ExtractListWithStringValues("TestData/nodelimiterlist.txt", "") == ["1", "2", "3", "4", "5", "6"]
--- Utilities/InputParser.py
+++ Utilities/InputParser.py
@@ -1,5 +1,8 @@
-def ExtractList(relativeFileLocation, delimiter):
+def ExtractListWithIntValues(relativeFileLocation, delimiter):
+    inputAsListOfStrings = ExtractListWithStringValues(relativeFileLocation, delimiter)
+    return [int(x) for x in inputAsListOfStrings]
+
+def ExtractListWithStringValues(relativeFileLocation, delimiter):
     inputFile = open(relativeFileLocation, "r")
     inputAsString = inputFile.readline()
-    inputAsListOfStrings = inputAsString.split(delimiter) if delimiter != "" else list(inputAsString)
-    return [int(x) for x in inputAsListOfStrings]
+    return inputAsString.split(delimiter) if delimiter != "" else list(inputAsString)
