711035fb0441de2c0aead7143bbe19ed0f40205f
Stephen Watson <svenjwatson@gmail.com>
2023-12-01 23:21:31 +1030

    (Day 1 Part 1) Part 1 tests pass

--- Tests/Day1Tests.py
+++ Tests/Day1Tests.py
@@ -1,18 +1,20 @@
 from Challenge.Day1 import Day1Part1
+import Utilities.InputParser
 
 TESTDATALOCATION = "../Tests/TestData/Day1Part1TestData"
 
-def test_GetDataAsAListOfStrings():
-    assert Day1Part1.GetInputDataAsListOfStrings(TESTDATALOCATION) == ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
-
 def test_FindFirstCalibrationOnFirstLine():
-    inputData = Day1Part1.GetInputDataAsListOfStrings(TESTDATALOCATION)
+    inputData = Utilities.InputParser.ExtractListOfStringsFromLinesOfStrings(TESTDATALOCATION)
     assert Day1Part1.GetFirstValueFromInputLine(inputData[0]) == 1
 
 def test_FindLastCalibrationOnFirstLine():
-    inputData = Day1Part1.GetInputDataAsListOfStrings(TESTDATALOCATION)
+    inputData = Utilities.InputParser.ExtractListOfStringsFromLinesOfStrings(TESTDATALOCATION)
     assert Day1Part1.GetLastValueFromInputLine(inputData[0]) == 2
 
 def test_GetResultFromFirstLine():
-    inputData = Day1Part1.GetInputDataAsListOfStrings(TESTDATALOCATION)
+    inputData = Utilities.InputParser.ExtractListOfStringsFromLinesOfStrings(TESTDATALOCATION)
     assert Day1Part1.GetResultFromInputLine(inputData[0]) == 12
+
+def test_Solve():
+    assert Day1Part1.Solve(TESTDATALOCATION) == 142
+
